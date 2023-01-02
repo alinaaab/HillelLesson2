92292d4f7ca1cdf8ea3d783147540696945a039f
Alina <alina@MacBook-Air-Alina.local>
2023-01-02 19:27:27 +0200

    First commit

---
+++ src/Coordinator.java
@@ -0,0 +1,13 @@
+public class Coordinator {
+    public static void main(String[] args) {
+        showCoordinates();
+    }
+
+    public static void showCoordinates() {
+        double latitude = 40.6892;
+        double longtitude = 74.0445;
+
+        System.out.println(latitude);
+        System.out.println(longtitude);
+    }
+}
