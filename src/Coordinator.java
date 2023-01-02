8d1a4aab133f9ef7f4ab546b8fca1502355d8a27
Alina <alina@MacBook-Air-Alina.local>
2023-01-02 20:33:57 +0200

    My first commit

--- src/Coordinator.java
+++ src/Coordinator.java
@@ -2,12 +2,11 @@ public class Coordinator {
     public static void main(String[] args) {
         showCoordinates();
     }
-
     public static void showCoordinates() {
-        double latitude = 40.6892;
-        double longtitude = 74.0445;
+        double latitude = 48.8584;
+        double longtitude = 2.2945;
 
         System.out.println(latitude);
         System.out.println(longtitude);
-    }
+        }
 }
