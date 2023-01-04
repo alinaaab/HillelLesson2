d7dbf56fc0161738a0b82bcd46118a7eda9f7bc3
Alina <alina@MacBook-Air-Alina.local>
2023-01-04 19:48:48 +0200

    My first commit

---
+++ src/Volume.java
@@ -0,0 +1,14 @@
+public class Volume {
+    public static void main(String[] args) {
+
+        int a = 8;
+        int b = 5;
+        int c = 3;
+
+        int volume = a * b * c;
+        System.out.println(volume);
+
+        int length = (a + b + c) * 4;
+        System.out.println(length);
+    }
+}
